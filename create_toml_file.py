8a163dd70bc83d0abf6324c6289badb77c1d76b6
Oleg Egorov <oleg.egorov@uni-heidelberg.de>
2025-12-01 22:37:58 +0100

    Updated script for creating TOML file from the query relying on drpall

--- create_toml_file.py
+++ create_toml_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-import sys, os
+import sys
+import os
 import logging
 import json
 import numpy as np
 import requests
 from requests.structures import CaseInsensitiveDict
+from astropy.io import fits
+from astropy.table import Table
 
 log = logging.getLogger(name='CreateTomlFile')
 log.setLevel(logging.DEBUG)
@@ -16,11 +19,15 @@ server_group_id = 10699
 url_json_file = "https://raw.githubusercontent.com/sdss/lvmvis/refs/heads/main/frontend/src/assets/lvm.json"
 local_json_file = '/data/LVM/lvm.json'
 use_local_file = False
-drp_version = '1.1.1'
-dap_version = '1.0.1'
-dap_local_version = '1.1.1dev'
+drp_version = '1.2.0'
+dap_version = '1.2.0'
+dap_local_version = '1.2.0dev'
+drpall_file = os.path.join(os.environ.get('SAS_BASE_DIR'),
+                      'sdsswork/lvm/spectro/redux', drp_version, 'drpall-{drp_version}.fits')
+if not os.path.exists(drpall_file):
+    drpall_file = None
 
-def parse_tiles(request, file_save=None):
+def parse_tiles(request, file_save=None, drpall=False):
     try:
         request = request.lower()
         request = request.replace('and', '&')
@@ -29,43 +36,57 @@ def parse_tiles(request, file_save=None):
         log.error(f"Something wrong with parsing request: {e}")
         return
 
-    try_local = False
-    if os.environ.get('GITHUB_TOKEN') is not None:
-        access_token = os.environ.get('GITHUB_TOKEN')
-        try:
-            headers = CaseInsensitiveDict()
-            headers["Authorization"] = f"token {access_token}"
-            resp = requests.get(url_json_file, headers=headers)
-            parsed_json = json.loads(resp.content)
-            all_frames = parsed_json["data"]
-            meta = parsed_json["meta"]
-        except Exception as e:
-            log.error(f"Something wrong with access to online json file with your GitHub token: {e}")
-            try_local = True
-    else:
-        log.warning(f"Cannot access the latest LVM dump file in lvmvis "
-                    f"without your GITHUB_TOKEN as environmental variable")
-        try_local = True
-    if try_local:
-        log.info(f"Trying local file: {local_json_file}")
-        try:
-            f = open(local_json_file, 'r')
-            json_string = f.read()
-            f.close()
-            parsed_json = json.loads(json_string)
-            all_frames = parsed_json["data"]
-            meta = parsed_json["meta"]
-        except Exception as e:
-            log.error(f"Something wrong with local json file: {e}. Cannot proceed.")
+    if drpall:
+        if drpall_file is None:
             return
-    log.info(f"Selecting from {len(all_frames)} frames from LVM dump generated on {meta['time_generated']}")
-    mjd = np.array([fr['mjd'] for fr in all_frames], dtype=int)
-    tile_id = np.array([fr['tile_id'] for fr in all_frames], dtype=int)
-    exposure = np.array([fr['exposure'] for fr in all_frames], dtype=int)
-    ra = np.array([fr['ra'] for fr in all_frames], dtype=float)
-    dec = np.array([fr['dec'] for fr in all_frames], dtype=float)
-    pa = np.array([fr['pa'] for fr in all_frames], dtype=float)
-    idx = np.array([fr['idx'] for fr in all_frames], dtype=int)
+        with fits.open(drpall_file) as hdul:
+            all_frames = Table.read(hdul[1])
+            idx = np.arange(len(all_frames))
+            kws = {'MJD': 'mjd', 'TILEID': 'tileid', 'EXPID': 'expnum',
+                   'RA': 'sci_ra', 'DEC': 'sci_dec', 'PA': 'sci_pa'}
+    else:
+        try_local = False
+        if os.environ.get('GITHUB_TOKEN') is not None:
+            access_token = os.environ.get('GITHUB_TOKEN')
+            try:
+                headers = CaseInsensitiveDict()
+                headers["Authorization"] = f"token {access_token}"
+                resp = requests.get(url_json_file, headers=headers)
+                parsed_json = json.loads(resp.content)
+                all_frames = parsed_json["data"]
+                meta = parsed_json["meta"]
+            except Exception as e:
+                log.error(f"Something wrong with access to online json file with your GitHub token: {e}")
+                try_local = True
+        else:
+            log.warning(f"Cannot access the latest LVM dump file in lvmvis "
+                        f"without your GITHUB_TOKEN as environmental variable")
+            try_local = True
+        if try_local:
+            log.info(f"Trying local file: {local_json_file}")
+            try:
+                f = open(local_json_file, 'r')
+                json_string = f.read()
+                f.close()
+                parsed_json = json.loads(json_string)
+                all_frames = parsed_json["data"]
+                meta = parsed_json["meta"]
+            except Exception as e:
+                log.error(f"Something wrong with local json file: {e}. Cannot proceed.")
+                return
+        log.info(f"Selecting from {len(all_frames)} frames from LVM dump generated on {meta['time_generated']}")
+
+        kws = {'MJD': 'mjd', 'TILEID': 'tile_id', 'EXPID': 'exposure',
+               'RA': 'ra', 'DEC': 'dec', 'PA': 'pa'}
+        idx = np.array([fr['idx'] for fr in all_frames], dtype=int)
+
+    mjd = np.array([fr[kws['MJD']] for fr in all_frames], dtype=int)
+    tile_id = np.array([fr[kws['TILEID']] for fr in all_frames], dtype=int)
+    exposure = np.array([fr[kws['EXPID']] for fr in all_frames], dtype=int)
+    ra = np.array([fr[kws['RA']] for fr in all_frames], dtype=float)
+    dec = np.array([fr[kws['DEC']] for fr in all_frames], dtype=float)
+    pa = np.array([fr[kws['PA']] for fr in all_frames], dtype=float)
+
 
     try:
         rec = np.flatnonzero(eval(request))
@@ -123,12 +144,15 @@ download_dap = false
 reduce_parallel = false # if true, then drp will run in parallel threads. This will use all processors on the server, so other users will not able to do anything. Avoid this unless you need this done urgently 
 wham_sky_only = false # set true if you want to include only sky from the darkest sky patch (wham).
 copy_cframes = false # Set true if CFrames should be copied to the object folder after reduction is done. By default, only SFrames will be copied 
+only_copy_files = false # Set true to skip reduction and only copy already reduced files to the work dir 
 
 # =======================================
 # === Control of imaging in individual lines
 # =======================================
 [imaging]
 use_dap = false
+fallback_fluxcal = false  # "SCI" (default), 'MOD', 'STD' or false (to skip) -> if problems detected with the current fluxcal, this fallback will be used instead (just normalization will be applied)
+force_calib = false # if true, then normalization to fallback_fluxcal will be applied to all exposures
 include_sky = false # if true, then subtracted sky will be added back before the flux extraction
 use_single_rss_file = false # if true, then creates single RSS file with all individual fibers and process this file.
 use_binned_rss_file = false # if true, then process binned RSS file.  
@@ -137,6 +161,7 @@ pxscale = 15 # Pixel scale (in arcsec) for output image
 sigma = 2 # arcsec; standard deviation for calculating of the weights of neighbouringfibers
 r_lim = 50 # arcsec; maximal radius to limit the fibers to be considered in flux calculation for a given position
 fiber_pos_precision = 1.5 # Spectra from the fibers with positions deviating by less than this value (in arcsec) are combined with sigma-clipping before the analysis
+lines_config = "lines_to_measure.toml"  # file with lines to measure (see example below)
 
 # =======================================
 # === Control for Voronoi binning
@@ -144,7 +169,10 @@ fiber_pos_precision = 1.5 # Spectra from the fibers with positions deviating by 
 [binning]
 use_binmap = false
 maps_source = 'maps'
-line = 'Ha'
+line = 'Ha_p'
+pxscale = 10 # Pixel scale (in arcsec) for the map used for evaluating S/N in bins
+rescale_noise = true # if true, then uses a tweak to upscale noise to make it consistent with the RSS spectra
+sn_prefilter = 0 # minimal S/N in pre-filtering of pixels before binning
 target_sn = 30
 correct_vel_line = 'Ha'
 mask_ds9_suffix = '_bin_exclude.reg'
@@ -163,147 +191,34 @@ file_output_suffix = '_extracted.fits'
 # =======================================
 [dap_fitting]
 fit_mode = 'rss' # can be 'extracted' (i.e. based on ds9-masks), 'binned' (i.e. extracted in voronoi bins) or 'rss' (i.e. huge single RSS file)
+skip_running_dap = false # if true, then assume dap results already exist and only extract the measurements (useful if there were external changes in DAP results)
 
 
 # =======================================
 # ====== Setup for lines to measure =====
 # =======================================
 
-# # # === Moment0 maps =====================
-[[imaging.lines]] # One block per line for output image
-line = 'Ha_mom0' # name of the line (arbitrary)
-wl_range = [6561, 6564] # rest-frame wavelength range for the line
-cont_range = [6530, 6540, 6605, 6620]  # rest-frame wavelength range for the continuum (w00, w01, w10, w11)
+# !!! It is better to store line list in a separate file and include it here !!!
+# !!! This config assumes there is a file 'lines_to_measure.toml' with the content similar to below. 
+# !!! Also, it can be uncommented from here.
+# # # # === Moment0 maps =====================
+# [[imaging.lines]] # One block per line for output image
+# line = 'Ha_mom0' # name of the line (arbitrary)
+# wl_range = [6561, 6564] # rest-frame wavelength range for the line
+# cont_range = [6530, 6540, 6605, 6620]  # rest-frame wavelength range for the continuum (w00, w01, w10, w11)
+# 
+# # # === Gaussian fits =====================
+# 
+# [[imaging.lines]]
+# line = ["Ha", "NII6584"]
+# wl_range = [6523, 6610]
+# line_fit = [6562.78, 6583.5, 6548.1]
+# fix_ratios = [-1, 1, 0.333]
+# tie_disp = [false, true, true]
+# tie_vel = [false, true, true]
+# include_comp = [0, 1, -1] # -1 means not to include this component in the output
+# filter_sn = 5 # minimal signal-to-noise ratio to consider during imaging
 
-[[imaging.lines]]
-line = 'OIII5007_mom0'
-wl_range = [5003, 5010]
-cont_range = [4831, 4851, 5030, 5050]
-
-[[imaging.lines]] 
-line = 'SIIsum_mom0'
-wl_range = [6713, 6721, 6727, 6735] # Sum both lines
-cont_range = [6640, 6665, 6745, 6770]
-
-[[imaging.lines]]
-line = 'NII6584_mom0'
-wl_range = [6581, 6586]
-cont_range = [6530, 6542, 6620, 6640]
-
-[[imaging.lines]]
-line = 'Hb_mom0'
-wl_range = [4858, 4865]
-cont_range = [4820, 4840, 4875, 4890]
-
-[[imaging.lines]]
-line = 'Hg_mom0'
-wl_range = [4337, 4343]
-cont_range = [4415, 4335, 4345, 4355]
-
-[[imaging.lines]]
-line = 'HeII_mom0'
-wl_range = [4678, 4694]
-cont_range = [4590, 4620, 4740, 4780]
-
-[[imaging.lines]]
-line = 'OIIsum_mom0'
-wl_range = [3724, 3732]
-cont_range = [3710, 3720, 3735, 3750]
-
-[[imaging.lines]]
-line = 'SIII9532_mom0'
-wl_range = [9529, 9535]
-cont_range = [9500, 9520, 9540, 9560]
-
-[[imaging.lines]]
-line = 'SIII6312_mom0'
-wl_range = [6309, 6315]
-cont_range = [6270, 6290, 6320, 6340]
-
-[[imaging.lines]]
-line = 'OIII4363_mom0'
-wl_range = [4361, 4366]
-cont_range = [4320, 4335, 4375, 4395]
-
-[[imaging.lines]]
-line = 'NII5755_mom0'
-wl_range = [5752, 5758]
-cont_range = [5725, 5745, 5762, 5778]
-
-# # === Gaussian fits =====================
-
-[[imaging.lines]]
-line = ["Ha", "NII6584"]
-wl_range = [6523, 6610]
-line_fit = [6562.78, 6583.5, 6548.1]
-fix_ratios = [-1, 1, 0.333]
-tie_disp = [false, true, true]
-tie_vel = [false, true, true]
-include_comp = [0, 1, -1] # -1 means not to include this component in the output
-filter_sn = 5 # minimal signal-to-noise ratio to consider during imaging
-
-[[imaging.lines]]
-line = ["OIII5007"]
-wl_range = [4930, 5030]
-line_fit = [4958.9, 5006.8]
-# max_comps = [2, 2]  # maximal number of components to fit, if needed more than one
-fix_ratios = [0.333, 1]
-include_comp = [-1, 0]
-
-[[imaging.lines]]
-line = ["Hb"]
-wl_range = [4840, 4880]
-line_fit = [4861.3]
-include_comp = [0]
-
-[[imaging.lines]]
-line = ["Hg", "OIII4363"]
-wl_range = [4325, 4380]
-line_fit = [4340.5, 4363.2]
-include_comp = [0, 1]
-filter_sn = 1
-
-[[imaging.lines]]
-line = ["OII3726", "OII3729"]
-wl_range = [3700, 3750]
-line_fit = [3726.0, 3728.8]
-include_comp = [0, 1]
-
-[[imaging.lines]]
-line = ["SII6717", "SII6731"]
-wl_range = [6680, 6760]
-line_fit = [6716.4, 6730.8]
-include_comp = [0, 1]
-
-[[imaging.lines]]
-line = ["NII5755"]
-wl_range = [5730, 5780]
-line_fit = [5754.64]
-include_comp = [0]
-
-[[imaging.lines]]
-line = ["SIII6312"]
-wl_range = [6306, 6325]
-line_fit = [6312.1]
-include_comp = [0]
-
-[[imaging.lines]]
-line = ["OII7320", 'OII7330']
-wl_range = [7300, 7350]
-line_fit = [7320.0, 7330.0]
-include_comp = [0, 1]
-
-[[imaging.lines]]
-line = ["HeII"]
-wl_range = [4670, 4700]
-line_fit = [4685.7]
-include_comp = [0]
-
-[[imaging.lines]]
-line = 'SIII9532'
-wl_range = [9500, 9560]
-line_fit = [9531.1]
-include_comp = [0]
 
 # =======================================
 # === Limits For cube reconstruction, if needed =====
@@ -353,14 +268,22 @@ suffix = 'OIII5007'
 
 if __name__ == "__main__":
     args = sys.argv
+    drpall = False
+    positional = []
     if len(args) < 2:
         log.error("Please provide valid request from lvmvis tool")
 
     else:
-        request = args[1]
-        if len(args) == 2:
-            file_save = None
-        else:
-            file_save = args[2]
+        for arg in args[1:]:
+            if arg == "--drpall":
+                drpall = True
+            else:
+                positional.append(arg)
+        # Check required argument: query
+        if len(positional) < 1:
+                raise ValueError("Missing required argument: query")
+
+        request = positional[0]
+        file_save = positional[1] if len(positional) > 1 else None
         log.info(f"Generating TOML config file for LVM processing using the request: {request}")
-        parse_tiles(request, file_save)
+        parse_tiles(request, file_save, drpall=drpall)
